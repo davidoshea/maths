e0c13e43ce172efb3e1d7c503d4702a2ea2408f4
davidoshea <44163829+davidoshea@users.noreply.github.com>
2024-12-12 21:55:23 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,21 @@
-basic.forever(function () {
-	
-})
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
-    let originalsum = 100000000000
-    let sum = originalsum
-    let denom:number  = 1
-    for (let index = 1; index <= 99999999; index++) {
-        denom = denom * index
-        sum = sum +  (originalsum / denom) 
+    originalsum = 10000
+    sum = originalsum
+    for (let index = 0; index <= 5; index++) {
+        sum += (originalsum / originalsum * (index + 1))
+        basic.showString("< ")
+        basic.showNumber(sum)
+        basic.showString(" >")
+        basic.clearScreen()
     }
-    basic.showString("< ")
-    basic.showNumber(sum)
-    basic.showString(" >")
-    basic.clearScreen()
+})
+function doSomething (text: string) {
+    basic.showString("doSomething")
+}
+let sum = 0
+let originalsum = 0
+let denom = 0
+basic.forever(function () {
+    doSomething
 })
